de602d6538788c6c8e7080b1bb1e3f24dda05a76
MinhThan25 <quanhnhoc@gmail.com>
2025-05-29 23:21:40 +0700

    add how to connect the database sql server

--- connect.js
+++ connect.js
@@ -1,16 +1,15 @@
 const sql = require("mssql");
-
+require("dotenv").config();
 const config = {
-  server: "THANITLO",
-  user: "sa",
-  password: "123123",
-  database: "MSSQL_ONLINE_MEDICAL_BOOKING",
+  server: process.env.DB_SERVER,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   options: {
     trustServerCertificate: true,
     enableArithAbort: true,
   },
 };
-
 const pool = new sql.ConnectionPool(config);
 const conn = pool.connect();
 
